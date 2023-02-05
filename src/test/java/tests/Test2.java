7128ec57f0224464696641197df9da40b99c610c
kalamgit143 <magnitiait@gmail.com>
2023-02-05 11:25:21 +0530

    developed reusable methods

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -2,11 +2,24 @@ package tests;
 
 public class Test2
 {
+	public int add(int x, int y)
+	{
+		int z=x+y;
+		return(z);
+	}
+	public static int subtract(int x, int y)
+	{
+		int z=x-y;
+		return(z);
+	}
 	public static void main(String[] args)
 	{
 		int x=10;
 		int y=20;
-		int z=x+y;
+		Test2 obj=new Test2();
+		int z=obj.add(x, y); //calling non-static method
+		System.out.println(z);
+		z=Test2.subtract(x, y); //calling static method
 		System.out.println(z);
 	}
 }
