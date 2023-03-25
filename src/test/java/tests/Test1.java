379cd468f66d3f532d7e24e84dbcbd1a19543992
kalamgit143 <magnitiait@gmail.com>
2023-03-25 11:37:17 +0530

    fhfjdgjg

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,7 +15,7 @@ public class Test1
 	{
 		//open browser(SWD-Java)
 		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver=new ChromeDriver();
+		RemoteWebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		Assert.assertTrue(true);
 		driver.quit();
